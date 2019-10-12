56c6aafd0aca3da57f0e430233dfbf94d68c0945
Schalldampfer <ahtosi.game@gmail.com>
2019-10-12 11:15:06 +0900

    Update Medical.hpp
    less medicines

--- SQF/Missions/dayz_1337.chernarus/dayz_code/Configs/CfgLoot/Groups/Medical.hpp
+++ SQF/Missions/dayz_1337.chernarus/dayz_code/Configs/CfgLoot/Groups/Medical.hpp
@@ -1,37 +1,37 @@
 MedicalLow[] =
 {
 	{Loot_MAGAZINE,		8,		ItemBandage},
-	{Loot_MAGAZINE,		5,		ItemPainkiller},
-	{Loot_MAGAZINE,		4,		ItemMorphine},
-	{Loot_MAGAZINE,		3,		ItemEpinephrine},
+	{Loot_MAGAZINE,		1,		ItemPainkiller},
+	{Loot_MAGAZINE,		1,		ItemMorphine},
+	{Loot_MAGAZINE,		1,		ItemEpinephrine},
 	{Loot_MAGAZINE,		4,		ItemAntibacterialWipe},
-	{Loot_MAGAZINE,		4,		ItemHeatPack}
+	{Loot_MAGAZINE,		1,		ItemHeatPack}
 };
 
 MedicalHigh[] =
 {
-	{Loot_MAGAZINE,		5,		ItemMorphine},
-	{Loot_MAGAZINE,		3,		ItemBloodTester},
-	{Loot_MAGAZINE,		5,		ItemHeatPack},
-	{Loot_MAGAZINE,		2,		ItemBloodbagEmpty},
+	{Loot_MAGAZINE,		2,		ItemMorphine},
+	{Loot_MAGAZINE,		2,		ItemBloodTester},
+	{Loot_MAGAZINE,		2,		ItemHeatPack},
+	{Loot_MAGAZINE,		1,		ItemBloodbagEmpty},
 	
 	{Loot_GROUP,		2,		Antibiotics},
-	{Loot_GROUP,		6,		Bloodbags},
+	{Loot_GROUP,		1,		Bloodbags},
 	
-	{Loot_GROUP,		7,		MedicalLow}
+	{Loot_GROUP,		5,		MedicalLow}
 };
 
 MedicalBox[] =
 {
-	{Loot_MAGAZINE,		5,		ItemMorphine},
+	{Loot_MAGAZINE,		2,		ItemMorphine},
 	{Loot_MAGAZINE,		3,		ItemBloodTester},
-	{Loot_MAGAZINE,		5,		ItemHeatPack},
+	{Loot_MAGAZINE,		3,		ItemHeatPack},
 	{Loot_MAGAZINE,		2,		ItemBloodbagEmpty},
 	
 	{Loot_GROUP,		1,		Antibiotics},
 	{Loot_GROUP,		10,		Bloodbags},
 	
-	{Loot_GROUP,		7,		MedicalLow}
+	{Loot_GROUP,		2,		MedicalLow}
 };
 
 Antibiotics[] =
